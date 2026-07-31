bd4858651fcd1c305a8a5db6b4b00194660253e4
maaz80 <maazforlap@gmail.com>
2026-07-31 12:06:15 +0530

    Fixings

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(helmet({
 
 // CORS Configuration
 const allowedOrigins = [
-     // "http://localhost:3000",
+     "http://localhost:3000",
      "https://weekend-ux-user.netlify.app",
      "https://weekend-ux-admin.netlify.app",
 ].filter(Boolean);
